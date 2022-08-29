be1a77e3891a41138142fb50007e58fe775b7529
alikamranli <kamranlil324@gmail.com>
2022-08-29 11:07:37 +0400

    .

--- admin/routes.py
+++ admin/routes.py
@@ -1,3 +1,4 @@
+
 from flask import render_template, request, redirect, url_for, flash,jsonify
 from admin import admin_bp
 from admin.forms import CustomerForm, ProductForm
@@ -9,10 +10,19 @@ def admin_index():
 
 
 
-@admin_bp.route('/products')
+@admin_bp.route('/products', methods=['GET','POST'])
 def admin_products():
     from start import db
     from models import Product
     products=Product.query.all()
-    productForm=ProductForm
+    productForm=ProductForm()
+
+    if request.method=="POST":
+        brand=productForm.brand.data
+        model=productForm.model.data
+        description=productForm.description.data
+        product=Product(brand,model,description)
+        db.session.add(product)
+        db.session.commit()
+        return redirect('/admin/products')  
     return render_template('admin/products.html',productForm=productForm,products=products)
